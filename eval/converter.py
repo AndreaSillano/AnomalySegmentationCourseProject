c2d45359ecaa38dce0e59ffc03f5493a14c9793c
AndreaSillano <andreasillano@gmail.com>
2024-02-02 11:36:57 +0100

    updated dataset labels

---
+++ eval/converter.py
@@ -0,0 +1,59 @@
+import os, glob, sys
+from pathlib import Path
+# cityscapes imports
+
+from cityscapesscripts.helpers.csHelpers import printError
+from cityscapesscripts.preparation.json2labelImg import json2labelImg
+
+# The main method
+def main():
+    # Where to look for Cityscapes
+    if 'CITYSCAPES_DATASET' in os.environ:
+        cityscapesPath = os.environ['CITYSCAPES_DATASET']
+    else:
+        cityscapesPath = os.path.join('eval/Cityscapes')
+    # how to search for all ground truth
+    print(cityscapesPath)
+    searchFine   = os.path.join( cityscapesPath , "gtFine"   , "*" , "*" , "*_gt*_polygons.json" )
+    #searchCoarse = os.path.join( cityscapesPath , "gtCoarse" , "*" , "*" , "*_gt*_polygons.json" )
+    searchFinePath = Path(cityscapesPath)
+    print(searchFinePath.exists())
+    print(searchFine)
+    # search files
+    filesFine = glob.glob( searchFine )
+    filesFine.sort()
+    #filesCoarse = glob.glob( searchCoarse )
+    #filesCoarse.sort()
+    files = filesFine
+    # concatenate fine and coarse
+    #files = filesFine + filesCoarse
+    # files = filesFine # use this line if fine is enough for now.
+
+    # quit if we did not find anything
+    if not files:
+        printError( "Did not find any files. Please consult the README." )
+
+    # a bit verbose
+    print("Processing {} annotation files".format(len(files)))
+
+    # iterate through files
+    progress = 0
+    print("Progress: {:>3} %".format( progress * 100 / len(files) ), end=' ')
+    for f in files:
+        # create the output filename
+        dst = f.replace( "_polygons.json" , "_labelTrainIds.png" )
+
+        # do the conversion
+        try:
+            json2labelImg( f , dst , "trainIds" )
+        except:
+            print("Failed to convert: {}".format(f))
+            raise
+
+        # status
+        progress += 1
+        print("\rProgress: {:>3} %".format( progress * 100 / len(files) ), end=' ')
+        sys.stdout.flush()
+
+
+main()
--- eval/dataset.py
+++ eval/dataset.py
@@ -70,11 +70,9 @@ class cityscapes(Dataset):
         self.labels_root = os.path.join(root, 'gtFine/' + subset)
         print(self.images_root, self.labels_root)
         self.filenames = [os.path.join(dp, f) for dp, dn, fn in os.walk(os.path.expanduser(self.images_root)) for f in fn if is_image(f)]
-        print(len(self.filenames))
         self.filenames.sort()
 
         self.filenamesGt = [os.path.join(dp, f) for dp, dn, fn in os.walk(os.path.expanduser(self.labels_root)) for f in fn if is_label(f)]
-        print(len(self.filenamesGt))
         
         self.filenamesGt.sort()
 
