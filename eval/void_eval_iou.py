3d1c6cf919637fa28726d1474398663e1068c10b
AndreaSillano <andreasillano@gmail.com>
2024-02-12 12:13:11 +0100

    update index

--- eval/void_eval_iou.py
+++ eval/void_eval_iou.py
@@ -105,7 +105,7 @@ def main(args):
     loader = DataLoader(cityscapes(args.datadir, input_transform_cityscapes, target_transform_cityscapes, subset=args.subset), num_workers=args.num_workers, batch_size=args.batch_size, shuffle=False)
 
 
-    iouEvalVal = iouEval(NUM_CLASSES, ignoreIndex=19)
+    iouEvalVal = iouEval(NUM_CLASSES, ignoreIndex=20)
 
     start = time.time()
 
