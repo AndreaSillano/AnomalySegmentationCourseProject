4dca38ec7b361061f2722fca813543218cbaad4b
AndreaSillano <andreasillano@gmail.com>
2024-02-13 15:52:14 +0100

    update

--- eval/temperature_scaling.py
+++ eval/temperature_scaling.py
@@ -2,7 +2,6 @@ import torch
 from torch import nn, optim
 from torch.nn import functional as F
 
-
 class ModelWithTemperature(nn.Module):
     """
     A thin decorator, which wraps a model with temperature scaling
