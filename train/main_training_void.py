b53f1958a5356d984ddab9e5b128d3f76c9ead39
AndreaSillano <andreasillano@gmail.com>
2024-02-15 16:25:05 +0100

    update

--- train/main_training_void.py
+++ train/main_training_void.py
@@ -288,8 +288,20 @@ def train(args, model, weight,dataset_train,dataset_val, enc=False):
             assert os.path.exists(filenameCheckpoint), "Error: resume option was used but checkpoint was not found in folder"
             #print ("Loading model: " + modelpath)
             #print ("Loading weights: " + weightspath)
-            state_dict = torch.load(filenameCheckpoint)
-            model.load_state_dict(state_dict)
+            def load_my_state_dict(model, state_dict):  #custom function to load model when not all dict elements
+                own_state = model.state_dict()
+                for name, param in state_dict.items():
+                    if name not in own_state:
+                        if name.startswith("module."):
+                            own_state[name.split("module.")[-1]].copy_(param)
+                        else:
+                            print(name, " not loaded")
+                            continue
+                    else:
+                        own_state[name].copy_(param)
+                return model
+
+            model = load_my_state_dict(model, torch.load(filenameCheckpoint, map_location=lambda storage, loc: storage))
             print("=> Loaded trained Model Bisenetv1")
 
     #scheduler = lr_scheduler.ReduceLROnPlateau(optimizer, 'min', factor=0.5) # set up scheduler     ## scheduler 1
